3382483556e1c974b1d6ad541e5c3b0bd3838986
brolasthai <brolasthai@gmail.com>
2025-12-19 10:19:49 +0530

    updated

--- supabase/migrations/20251114122711_a5175136-4831-4467-9ecd-f19496f32929.sql
+++
@@ -1,5 +0,0 @@
--- Create trigger to send OneSignal notifications when articles are published
-CREATE TRIGGER trigger_send_onesignal_notification
-  AFTER INSERT OR UPDATE ON public.articles
-  FOR EACH ROW
-  EXECUTE FUNCTION public.trigger_onesignal_notification();
--- supabase/migrations/20251203143936_02febba7-8aba-4978-810f-43feb1756094.sql
+++
@@ -1,8 +0,0 @@
--- Fix publisher logo to be square (Google requirement for Discover)
-UPDATE web_stories_config 
-SET 
-  publisher_logo_url = 'https://www.thebulletinbriefs.in/tb-briefs-favicon.png',
-  publisher_logo_width = 96,
-  publisher_logo_height = 96,
-  updated_at = now()
-WHERE id = 'adb5b2fe-a70a-414c-b33d-33833e178daf';
